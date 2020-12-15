d7e75c7ac5f1d04acedb179cfa279c482b6e4bbe
yeramLim <ramii1005@daum.net>
2020-12-15 20:17:40 +0900

    프로젝트

---
+++ spring_pjt/src/main/java/org/zerock/sample/Chef.java
@@ -0,0 +1,13 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data // Lombok의 setter를 생성하는 기능과 생성자, toString() 등을 자동으로 생성하는 @
+public class Chef {
+	
+	
+
+}
